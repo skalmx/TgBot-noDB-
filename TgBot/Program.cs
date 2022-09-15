e60e26e0ba74e76af12562b3af0e1a74ea8f35cb
skalmx <poplevinwork@gmail.com>
2022-09-15 13:59:15 +0300

    Добавлена тестовая экранная клавиатура

---
+++ TgBot/Program.cs
@@ -0,0 +1,73 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types.ReplyMarkups;
+
+ITelegramBotClient botClient = new TelegramBotClient("5521913177:AAFrPUb0CMxHB0zxXWnsr77WlueIh-tlBXw");
+
+using var cancellationTokenSource = new CancellationTokenSource();
+
+var receiverOptions = new Telegram.Bot.Extensions.Polling.ReceiverOptions
+{
+    AllowedUpdates = {} 
+};
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    cancellationToken: cancellationTokenSource.Token
+    );
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+cancellationTokenSource.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    //https://win10tweaker.ru/forum/topic/как-это-работает
+    if (update.Message is not { } message) // is not null message 
+       return;
+  
+    if (message.Text is not { } messageText) // is not null message Text
+       return;
+
+    ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
+{
+    new KeyboardButton[] { "Exapmple1", "Exapmple2" }
+})
+    {
+        ResizeKeyboard = true
+    };
+
+
+    if (message.Text.ToLower() == "/start")
+    {
+        await botClient.SendTextMessageAsync(
+            chatId: message.Chat.Id,
+            text: "Привет, рад тебя видеть! Как я могу тебе помочь?",
+            disableNotification: true,
+            replyMarkup: replyKeyboardMarkup,
+            cancellationToken: cancellationTokenSource.Token
+            ) ;
+         return;
+    }
+    await botClient.SendTextMessageAsync(message.Chat.Id, "Да я до сих пор в разработке");
+    
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}

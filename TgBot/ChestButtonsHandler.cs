be0ee8100887998c23e875e624bfb8250a5b5de8
skalmx <poplevinwork@gmail.com>
2022-11-08 15:28:14 +0300

    add test realization of video-stream

--- TgBot/ChestButtonsHandler.cs
+++ TgBot/ChestButtonsHandler.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using System.IO;
+using System.Reflection;
 
 namespace TgBot
 {
@@ -20,7 +22,18 @@ namespace TgBot
         }
         public static async void BenchPress(ITelegramBotClient botClient, Chat chat)
         {
-            await botClient.SendTextMessageAsync(chat.Id, "жим лежа");
+            await botClient.SendTextMessageAsync(chat.Id, "жим лежа \n123 тест");
+            Assembly assembly = Assembly.GetExecutingAssembly();
+           
+            using (Stream resourceStream = assembly.GetManifestResourceStream(@"TgBot.videos.Жим лежа.mp4")!)
+            {
+                await botClient.SendVideoAsync(
+                    chatId: chat.Id,
+                    video: resourceStream!,
+                    supportsStreaming: true
+                    );
+            }
+            
         }
         public static async void Dips(ITelegramBotClient botClient, Chat chat)
         {
